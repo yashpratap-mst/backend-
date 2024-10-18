a965d5a2dcd5c558d39f0ae371f231203849a81b
yashpratap-mst <yash.singh@metasquaretech.com>
2024-10-18 12:25:40 +0530

    updated custom api

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.use(
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
-app.use(cookieparser())
+app.use(cookieParser())
 
 export { app };
